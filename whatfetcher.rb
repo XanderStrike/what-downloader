bc474224a5fcaf0912b4148f16c5d8f1d2cfdd40
Alexander Standke <xanderstrike@gmail.com>
2014-07-08 11:29:57 -0700

    remove unnecessary comments

--- whatfetcher.rb
+++ whatfetcher.rb
@@ -18,12 +18,12 @@ mech = Mechanize.new
 
 # Login
 page = mech.get('http://what.cd/')
-page = mech.click page.link_with(text: /Log In/i) # Click the login link
+page = mech.click page.link_with(text: /Log In/i)
 form = page.forms[0]
 form.set_fields({username: ARGV[0], password: ARGV[1]})
 page = form.submit form.buttons.first
 
-page = mech.click page.link_with(:text => /Torrents/i) # Click the login link
+page = mech.click page.link_with(:text => /Torrents/i)
 
 # Download the torrents
 page.links_with(text: 'DL').each do |l|
